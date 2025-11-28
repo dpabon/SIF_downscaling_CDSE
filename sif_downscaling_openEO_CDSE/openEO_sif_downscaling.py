0526c2e340d80395beb03e58dcedf38cf397306d
dpabon <13040959+dpabon@users.noreply.github.com>
2025-11-28 10:07:48 +0100

    cells removed, now using positron

--- sif_downscaling_openEO_CDSE/openEO_sif_downscaling.py
+++ sif_downscaling_openEO_CDSE/openEO_sif_downscaling.py
@@ -1,18 +1,15 @@
-# %%
 import openeo
 import rioxarray as rio
 
-# %%
 connection = openeo.connect("https://openeofed.dataspace.copernicus.eu/")
-# %%
+
 connection.list_collections()
-# %%
+
 connection.list_processes()
 
-# %%
+
 connection.authenticate_oidc()
 
-# %%
 
 spatial_extent_prototype = {
     "west": 0.08431253773658387,
@@ -25,8 +22,6 @@ temporal_extent_prototype = ["2023-07-01", "2023-07-07"]
 
 temporal_extent_prototype = ["2018-06-20", "2018-06-30"]
 
-# %%
-
 # Testing SIF - Dong Li
 
 cube_SIF = connection.load_stac(
@@ -36,9 +31,9 @@ cube_SIF = connection.load_stac(
     bands=["SIF"],
 )
 
-# %%
+
 # cube_SIF.execute_batch(outputfile="openeo_test.tif", title="SIF", description="Testing SIF extraction", job_options={"image-name": "python311-staging"})
-# %%
+
 
 # Preparing Sentinel-3 data
 
@@ -62,10 +57,9 @@ cube_IWV = connection.load_collection(
     temporal_extent=temporal_extent_prototype,
     bands=["IWV"],
 )
-# %%
+
 cube_LST
 
-# %%
 # Collapsing time dimension
 
 cube_LST_median = cube_LST.reduce_temporal(reducer="median")
@@ -74,8 +68,8 @@ cube_OTCI_median = cube_OTCI.reduce_temporal(reducer="median")
 
 cube_IWV_median = cube_IWV.reduce_temporal(reducer="median")
 
+# Loading Dong Li SIF product
 
-# %%
 cube_SIF_original = connection.load_stac(
     "https://raw.githubusercontent.com/dpabon/SIF_downscaling_CDSE/refs/heads/main/data/SIF_20180629.json",
     bands=["SIF"],
@@ -83,11 +77,11 @@ cube_SIF_original = connection.load_stac(
     temporal_extent=temporal_extent_prototype,
 )
 
-# %%
+
 # cube_SIF_original_median = cube_SIF_original.reduce_temporal(reducer = "median")
 
 cube_SIF_original_median = cube_SIF_original
-# %%
+
 # resample cubes at SIF original resolution
 
 cube_LST_median_low = cube_LST_median.resample_cube_spatial(
@@ -108,7 +102,7 @@ cube_OTCI_median_low
 
 cube_IWV_median_low
 
-# %%
+
 # Merging all the cubes at low resolution
 
 dataset_SIF_low = cube_SIF_original_median.merge_cubes(cube_LST_median_low)
@@ -117,7 +111,7 @@ dataset_SIF_low = dataset_SIF_low.merge_cubes(cube_OTCI_median_low)
 
 dataset_SIF_low = dataset_SIF_low.merge_cubes(cube_IWV_median_low)
 
-# %%
+
 # adding two dummy variables to the datacube to match the number of oputput parameters
 dataset_SIF_low = dataset_SIF_low.merge_cubes(
     other=cube_SIF_original_median.rename_labels(dimension="bands", target=["dummy_1"])
@@ -125,8 +119,6 @@ dataset_SIF_low = dataset_SIF_low.merge_cubes(
 dataset_SIF_low = dataset_SIF_low.merge_cubes(
     other=cube_SIF_original_median.rename_labels(dimension="bands", target=["dummy_2"])
 )
-
-# %%
 
 # checking the output of the merge
 """
@@ -138,7 +130,7 @@ dataset_SIF_low.execute_batch(
 )
 """
 # dataset_SIF_low
-# %%
+
 # init parameters
 # param_ini = np.array([1.0, 2.0, -295.0, 10.0])
 # param_min = np.array([0.5, 0.1, -310.0, 1.0])
@@ -146,15 +138,15 @@ dataset_SIF_low.execute_batch(
 # param_bounds = np.array([param_min, param_max])
 
 
-# %%
+
 param_ini = [1, 2, 50.0, 0, -295, 10]
 param_min = [0.5, 0.1, 0.0, -1, -310, 1]
 param_max = [1.5, 5, 500.0, 1, -290, 50]
 
-# %%
+
 
 my_udf = openeo.UDF.from_file(
-    "udf/udf_parameters_optim_low_res.py",
+    "sif_downscaling_openEO_CDSE/udf/udf_parameters_optim_low_res.py",
     context={
         "param_ini": param_ini,
         "param_min": param_min,
@@ -166,13 +158,13 @@ my_udf = openeo.UDF.from_file(
 )
 
 
-# %%
+
 my_udf
 
-# %%
+
 # Checking the dimensions of the cube
 dataset_SIF_low
-# %%
+
 parameters_cube_low = dataset_SIF_low.apply_neighborhood(
     process=my_udf,
     size=[
@@ -184,45 +176,45 @@ parameters_cube_low = dataset_SIF_low.apply_neighborhood(
         {"dimension": "y", "value": 0, "unit": "px"},
     ],
 )
-# %% changing the name of the ouput
+# changing the name of the ouput
 output_bands = ["b1", "b2", "b3", "b4", "b5", "b6"]
 parameters_cube_low_rename = parameters_cube_low.rename_labels("bands", output_bands)
 parameters_cube_low_rename
-# %%
-# %%
+
+
 # Checking the ouput parameters
 job = parameters_cube_low_rename.execute_batch(
     output_file="openeo_sif_parameters.nc",
     title="SIF_parameters_computation",
     description="Testing SIF extraction",
 )
-# %%
+
 results = job.get_results()
 
-# %%
+
 results.download_file("../data/results_parameters_optim_low_resolution.tif")
-# %%
+
 # Downloading LST at high resolution to upsample the paramaters cube locally
 
 cube_LST_median.download("../data/lst_high_resolution.tif")
 
-# %%
+
 # Doing the upsampling locally while openEO dev reply...
 
 parameters_low = rio.open_rasterio(
     "../data/results_parameters_optim_low_resolution.tif"
 )
 parameters_low
-# %%
+
 # LST high resolution local
 lst_high = rio.open_rasterio("../data/lst_high_resolution.tif")
 lst_high
-# %%
+
 parameters_high = parameters_low.rio.reproject_match(lst_high)
 parameters_high
-# %%
+
 parameters_high.rio.to_raster("../data/results_paramaters_high_resolution.tif")
-# %%
+
 # Creating a STAC item for the upsampled parameters:
 import pystac
 from pystac.extensions.projection import ProjectionExtension
@@ -282,7 +274,7 @@ with open("../data/results_parameters_high_resolution.json", "w") as f:
 
 print(item.to_dict())
 
-# %%
+
 
 # This still doesn't work
 """
@@ -290,20 +282,20 @@ parameters_cube_high = parameters_cube_low_rename.resample_cube_spatial(
     target=cube_LST_median
 )
 
-# %%
 
-# %%
+
+
 parameters_cube_high = parameters_cube_low.resample_spatial(
     resolution=0.008928571428571, projection=None, method="bilinear"
 )
-# %%
+
 job = parameters_cube_high.execute_batch(
     outputfile="openeo_sif_parameters_high.nc",
     title="SIF",
     description="Testing SIF extraction",
 )
 """
-# %%
+
 # Loading the parameters at high resolution in openEO
 
 parameters_cube_high = connection.load_stac(
@@ -314,9 +306,9 @@ parameters_cube_high = connection.load_stac(
 )
 parameters_cube_high
 
-# %%
-parameters_cube_high_median= parameters_cube_high.reduce_temporal("median")
-# %%
+
+parameters_cube_high_median = parameters_cube_high.reduce_temporal("median")
+
 
 # resampling VI and ET at 1 km to match LST
 
@@ -325,24 +317,24 @@ cube_OTCI_median_1 = cube_OTCI_median.resample_cube_spatial(target=cube_LST_medi
 cube_IWV_median_1 = cube_IWV_median.resample_cube_spatial(target=cube_LST_median)
 
 
-# %%
+
 cube_to_upscale = parameters_cube_high_median.merge_cubes(cube_LST_median)
 
 cube_to_upscale = cube_to_upscale.merge_cubes(cube_OTCI_median_1)
 
 cube_to_upscale = cube_to_upscale.merge_cubes(cube_IWV_median_1)
 
-# %%
-#cube_to_upscale.execute_batch("../data/cube_to_upscale.nc")
 
-# %%
+# cube_to_upscale.execute_batch("../data/cube_to_upscale.nc")
+
+
 # predicting SIF at 1 km resolution
 
 udf_sif_prediction = openeo.UDF.from_file(
     "udf/udf_sif_downscaling.py",
     context={"window_size_lat": 3, "window_size_lon": 3},
 )
-# %%
+
 sif_downscaled = cube_to_upscale.apply_neighborhood(
     udf_sif_prediction,
     size=[
@@ -355,10 +347,10 @@ sif_downscaled = cube_to_upscale.apply_neighborhood(
     ],
 )
 
-# %%
-# removing bands that are not needed it 
+
+# removing bands that are not needed it
 sif_downscaled
-#%%
+
 
 # result = sif_downscaled.save_result(format = "GTiff")
 
@@ -367,6 +359,3 @@ job = sif_downscaled.execute_batch(
     title="SIF_downscaling",
     description="Testing SIF extraction",
 )
-
-
-# %%
